d44e39943dd7a577bba4d6207bdd4c4a9654b421
Andreas Bigger <abigger87@gmail.com>
2022-01-07 18:32:10 -0500

    almost

--- src/InfiniteMint.sol
+++ src/InfiniteMint.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.11;
 
 import {ERC721} from "solmate/tokens/ERC721.sol";
 
-contract MockERC721 is ERC721 {
+contract InfiniteMint is ERC721 {
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 
     function tokenURI(uint256) public pure virtual override returns (string memory) {}
