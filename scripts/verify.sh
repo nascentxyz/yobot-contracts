411ad0dcdde562cb5facd94dd36ade576e17b201
Andreas Bigger <abigger87@gmail.com>
2022-02-12 11:46:13 -0800

    :rocket: deployed

---
+++ scripts/verify.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+forge verify-contract ./src/YobotERC721LimitOrder.sol:YobotERC721LimitOrder 0x715da5e53526bEdaC9Bd96e8FDB7eFb185D1B6CA 0xc9ab63915c6738c8ce5ca245979203bfa3f2499f 500
